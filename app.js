8e48a8a32de0227447e26032af4d67ac487e033e
egorerkovich <erkovich.egor@yandex.by>
2024-01-06 01:32:28 +0300

    refactor(extended-1.2): add media alternatives

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
     deleteButtonImg.className="delete-task-img";
+    deleteButtonImg.alt="task removing arrow";
 
     label.innerText=taskString;
     label.className='label task';
